5429bb01a33244d1fb94257a60b2ddbc12637f40
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-01 18:37:53 +0600

    Code Created

--- DATA-STRUCTURE/Linked-List/struct-singly-list-insert-queue.cpp
+++ DATA-STRUCTURE/Linked-List/struct-singly-list-insert-queue.cpp
@@ -1,75 +1,67 @@
-#include<bits/stdc++.h>
 #include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
-    int data;
-    struct Node *next;
+struct Node{
+
+int data;
+struct Node *next;
+
 };
-
-struct Node *start;
 struct Node *rear;
-struct Node *temp;
+struct Node *front;
 
-rear=new Node();
-temp=new Node();
-start=new Node();
-
-int enqueue(int data)
+void enqueue(int data)
 {
-    if(start==NULL&&rear==NULL)
+    struct Node *temp;
+    temp=new Node();
+    temp->data=data;
+    temp->next=NULL;
+    if(rear==NULL&&front==NULL)
     {
-        start->next=NULL;
-        start->data=data;
-        start=rear;
-
+    rear=temp;
+    front=temp;
     }
-
     else
     {
-
-        start->next=temp;
-        temp->data=data;
-        temp->next=NULL;
-        rear=temp;
+    rear->next=temp;
+    rear=temp;
     }
+
 }
 
 void dequeue()
 {
-    if(start==NULL&&rear==NULL)
-    {
-        return;
-    }
-    else
-    {
-    temp=start;
-     start=start->next;
-    }
+    cout<<front->data<<endl;
+    front=front->next;
 }
 
-void display()
+void print()
 {
-while(start!=NULL)
-{
-    cout<<start->data<<endl;
-    start=start->next;
-}
+
+    while(front!=NULL)
+    {
+    cout<<front->data<<endl;
+    front=front->next;
+    }
 }
 
 int main()
 {
-
     enqueue(10);
     enqueue(20);
     enqueue(30);
     enqueue(40);
-    enqueue(50);
-    enqueue(60);
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    cout<<"Deleted:"<<endl;
     dequeue();
     dequeue();
     dequeue();
-    display();
+    dequeue();
+    cout<<endl<<"List:"<<endl;
+    print();
     return 0;
 }
