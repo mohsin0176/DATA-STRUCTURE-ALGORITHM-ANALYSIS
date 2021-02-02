14708b5364b39507f74ccec48c8d2b34cfacbafc
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-02 20:32:07 +0600

    code created

---
+++ DATA-STRUCTURE/recursion-fibonacci.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    if(n>=1)
+    {
+        return n*fact(n-1);
+    }
+    else
+        {
+        return 1;
+        }
+}
+int fib(int n)
+{
+    if(n>=3)
+    {
+        return fib(n-1)+fib(n-2);
+    }
+    else
+        {
+        return 1;
+        }
+}
+
+int main()
+{
+    cout<<fact(30)<<endl;
+    cout<<endl;
+    cout<<fib(30);
+    return 0;
+}
