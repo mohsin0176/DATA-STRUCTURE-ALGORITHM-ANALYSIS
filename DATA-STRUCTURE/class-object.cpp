c4b978d6b302baec45b8919fb3d53f46c9a5d031
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-02 20:08:23 +0600

    code created

---
+++ DATA-STRUCTURE/class-object.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Car
+{
+    public:
+    string name;
+    string color;
+    int weight;
+
+    Car(string n,string c,int w)
+    {
+        this->name=n;
+        this->color=c;
+        this->weight=w;
+    }
+    void print()
+    {
+        cout<<this->name<<endl;
+        cout<<this->color<<endl;
+        cout<<this->weight<<endl;
+    }
+};
+int main()
+{
+
+    Car c3("Tom","yellow",30);
+    Car c4("Jerry","black",20);
+    c3.print();
+    c4.print();
+    return 0;
+}
