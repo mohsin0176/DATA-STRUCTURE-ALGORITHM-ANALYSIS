ff7bf520d3e07653e7b4437399cf8b685f658e24
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-14 23:08:39 +0600

    Code Created

---
+++ operator-ret-value-pst.cpp
@@ -0,0 +1,32 @@
+ #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Count
+{
+private:
+	int value;
+public:
+	Count():value(5){}
+    Count operator ++ (int)
+        {
+        Count temp;
+        temp.value=++value;
+        return temp;
+        }
+
+	void display(){cout<<value<<endl;}
+
+};
+
+
+int main()
+{
+
+	Count count1;
+	count1++;
+	count1.display();
+
+
+	return 0;
+}
