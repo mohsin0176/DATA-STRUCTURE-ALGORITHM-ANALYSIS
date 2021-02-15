91f14afa840caa429e438a87b31fb85c6b5b2551
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-15 20:27:16 +0600

    Code created

---
+++ FF-04_1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class A
+{
+private:
+    int vara;
+    friend class B;
+ 
+public:
+    A():vara(100) {}  
+     
+    
+};
+
+class B
+{
+private:
+    int varb;
+ 
+public:
+    B():varb(50){} 
+   
+    int add(A a)
+    {   
+        
+        return a.vara+varb;
+    }
+      
+
+};
+
+
+
+int main()
+{
+
+
+    B b;
+    A a;
+    cout<<b.add(a);
+ 
+    return 0;
+}
