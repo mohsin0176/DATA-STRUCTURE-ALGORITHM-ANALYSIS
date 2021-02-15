9c332859e946c0f1a777b8bdc43e877c934a9e92
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-15 23:43:05 +0600

    Code Created

---
+++ T-05.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+template<class T1,class T2>
+T1 add(T1 a,T2 b)
+{
+    return a+b;
+}
+
+int main()
+{
+    cout<<add(20,10);
+    return 0;
+}
