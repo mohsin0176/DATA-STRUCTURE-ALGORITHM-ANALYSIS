2031f0430aff5ffb73bcb183aa3b2f987af3dd05
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-16 15:28:24 +0600

    Code Created

--- DATA-STRUCTURE/pointer.cpp
+++ DATA-STRUCTURE/pointer.cpp
@@ -1,16 +1,27 @@
-#include<bits/stdc++.h>
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-
-int p=5;
-int *x;
-x=&p;
-
-cout<<*x<<endl;;
-int *y;
-y=&x;
-cout<<*y<<endl;
-return 0;
+	int a=10;
+	int *ptr=&a;
+	cout<<a;
+	cout<<&a;
+	cout<<ptr;
+	cout<<*ptr;
+	int **qtr=&ptr;
+	cout<<qtr;
+	cout<<*qtr;
+	int arr[5]={5,10,20,1,45};
+	int *p=arr;
+	int i;
+	for(i=0;i<=4;i++)
+	{
+		cout<<(arr+i)<<endl;
+		cout<<*(arr+i)<<endl;
+		cout<<(p+i)<<endl;
+		cout<<*(p+i)<<endl;
+	}
+	return 0;
 }
